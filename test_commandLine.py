febd68136a65c6fac2a35e2fc2b9a026b8273a67
= <=>
2024-06-06 01:22:51 -0400

    Used helper functions to make code more modular

--- test_commandLine.py
+++ test_commandLine.py
@@ -1,44 +1,11 @@
-import requests
-from bs4 import BeautifulSoup, ResultSet
+import helpers
 from nlp_summarizer import summarize
-
-# create global topics_list variable
-topics_list: list[str] = ["tech", "space", "physics", "life", "humans", "earth"]
-
-def getArticles(topic: str) -> ResultSet:
-    """A function that returns a ResultSet of all the articles from ScienceNews.org that match a given topic parameter."""
-    # define variable to store sciencenews incomplete URL
-    website_url = "https://www.sciencenews.org/topic/"
-
-    # loop until user picks a valid topic
-    while topic not in topics_list:
-        print("Invalid topic.")
-        topic = input("Please pick a news article topic from tech, space, physics, life, humans, or earth. ")
-    
-    # concatenate url with valid topic
-    website_url += topic
-
-    # get raw webpage content
-    article_content = requests.get(website_url)
-
-    # check if html request was successful
-    if article_content.status_code == 200:
-        article_html = article_content.text
-    else:
-        raise Exception(f"failed to fetch content from {website_url}")
-
-    # parse html and extract articles
-    article_main_page = BeautifulSoup(article_html, "html.parser")
-
-    # get a set of the content of each article
-    article_list = article_main_page.find_all("h3", class_ = "post-item-river__title___vyz1w")
-    return article_list
 
 def test_commandLine() -> None:
     """Command line implementation for the summarize() and getArticles() functions"""
     # handle user input for article topic and get ResultSet
     choice: str = input("Please pick a news article topic from tech, space, physics, life, humans, or earth. ")
-    article_list = getArticles(choice.lower())
+    article_list = helpers.getArticles(choice.lower())
 
     # iterate through and set title url parameters of each article
     for article in article_list:
@@ -47,17 +14,8 @@ def test_commandLine() -> None:
         url = url_and_title["href"]
         title = url_and_title.text
 
-        # get raw webpage content 
-        article_content = requests.get(url)
-
-        # check request status
-        if article_content.status_code == 200:
-            article_html = article_content.text
-        else:
-            raise Exception(f"Failed to fetch content from {url}")
-
-        # parse article html and extract all text into one string
-        article_page = BeautifulSoup(article_html, "html.parser")
+        # use getContent to get article html, handle exceptions, and convert to BeautifulSoup
+        article_page = helpers.getContent(url)
 
         # since articles on sciencenews.org have two different classes for article text,
         # use a try catch to handle any NoneType exceptions by trying both class definitions
