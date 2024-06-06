38e7168b6b97b5c601f08c08f2f390870144217f
= <=>
2024-06-05 23:16:42 -0400

    Added docstring and fixed word weighted frequencies

--- nlp_summarizer.py
+++ nlp_summarizer.py
@@ -2,13 +2,14 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 import heapq
 
-def summarize(text: str) -> str:
-    # tokenize the text
+def summarize(long_text: str) -> str:
+    """Algorithm that summarizes long text into an 8 sentence summary using a 5 step process."""
+    # 1. tokenize the text
     stop_words = stopwords.words("english")
-    words = word_tokenize(text)
-    sentences = sent_tokenize(text)
+    words = word_tokenize(long_text)
+    sentences = sent_tokenize(long_text)
 
-    # create a frequency table to count each word
+    # 2. create a frequency table to count each word
     word_freqTable = dict()
     for word in words:
         word = word.lower()
@@ -18,8 +19,13 @@ def summarize(text: str) -> str:
             word_freqTable[word] += 1
         else:
             word_freqTable[word] = 1
+
+    # 3. define weighted value for each word from given text
+    max_freq = max(word_freqTable.values())
+    for word in word_freqTable:
+        word_freqTable[word] /= max_freq
     
-    # create a frequency table to count each sentence
+    # 4. create a frequency table to count each sentence
     sentence_freqTable = dict()
     for sentence in sentences:
         for word, freq in word_freqTable.items():
@@ -27,15 +33,9 @@ def summarize(text: str) -> str:
                 if sentence in sentence_freqTable:
                     sentence_freqTable[sentence] += freq
                 else:
-                    sentence_freqTable[sentence] = freq
-    
-    # define weighted value for each sentence from given text
-    sumVal = 0
-    for sentence in sentence_freqTable:
-        sumVal += sentence_freqTable[sentence]
-    sentence_avg = sumVal // len(sentence_freqTable)
+                    sentence_freqTable[sentence] = freq    
 
-    # use heap queue to extract top 8 largest weighted values and display as summary
+    # 5. use heap queue to extract top 8 largest weighted values and display as summary
     summary_top8 =  heapq.nlargest(8, sentence_freqTable, key = sentence_freqTable.get) 
     summary = " ".join(summary_top8)
     return summary
