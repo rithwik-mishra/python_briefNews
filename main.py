026deccee09865ee72fd50371ba4497614e34677
= <=>
2024-06-09 04:13:41 -0400

    Made aesthetic changes with paragraph sizing

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ from webbrowser import open_new
 
 def onClick() -> None:
     url, title, full_text = helpers.randomArticle()
-    summarized_text = summarize(full_text)
+
+    full_summary = summarize(full_text)
+    first_half = "      " + (" ".join(full_summary[0:3])) + "\n\n"
+    full_text = first_half + ("     " + " ".join(full_summary[3:7]))
+
     article_label.config(text=title)
-    url_label.config(text=url)
-    text_label.config(text=summarized_text)
+    text_label.config(text=full_text)
 
 
 # get initial random article url, title, and text and assign to global vars
 url, title, full_text = helpers.randomArticle()
 
 # get summary for full text
-summarized_text = summarize(full_text)
+full_summary = summarize(full_text)
+first_half = "    " + (" ".join(full_summary[0:3])) + "\n\n"
+full_text = first_half + ("         " + " ".join(full_summary[3:7]))
 
 # initializing tkinter window
 root = tk.Tk()
@@ -33,21 +38,21 @@ title_label = ttk.Label(root, text="BriefNews", font=("Algerian", 40), backgroun
 title_label.place(relx = 0.5, rely = 0.05, anchor="n")
 
 # article title label
-article_label = ttk.Label(root, text=title, font=("Georgia", 16), background="antique white", wraplength=400)
+article_label = ttk.Label(root, text=title, font=("Georgia", 16), background="antique white", wraplength=400, anchor="center")
 article_label.place(relx = 0.22, rely = 0.12)
 
 # article url label
-url_label = ttk.Label(root, text=url, font=("Georgia", 12), background="antique white", foreground="blue", cursor="hand2", wraplength=400)
+url_label = ttk.Label(root, text="Full Article", font=("Georgia", 12), background="antique white", foreground="blue", cursor="hand2")
 url_label.bind("<Button-1>", lambda a: open_new(url))
-url_label.place(relx = 0.22, rely = 0.28)
+url_label.place(relx = 0.43, rely = 0.28)
 
-# article text label
-text_label = ttk.Label(root, text= summarized_text, font=("Georgia", 10), background="antique white", wraplength=600)
+# article first half text label
+text_label = ttk.Label(root, text= "       " + full_text, font=("Georgia", 10), background="antique white", wraplength=600)
 text_label.place(relx = 0.08, rely = 0.35)
 
 # next article button
 next_button = tk.Button(root, text= "Next", font=("Georgia", 12), background="antique white", command= onClick)
-next_button.place(relx= 0.5, rely = 0.9)
+next_button.place(relx= 0.45, rely = 0.95)
 root.update()
 
 root.mainloop()
